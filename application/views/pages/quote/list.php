2b972108860a4995d70a5785ecc92e3bf0afafe1
AndyZheng <tianhua.zheng@theoutnet.cn>
2015-02-09 14:31:58 +0800

    quote

---
+++ application/views/pages/quote/list.php
@@ -0,0 +1,61 @@
+
+<div class="container-fluid ">
+		<div class='subnav'>
+			<ul class="nav navbar nav-pills">
+			  <li role="presentation" class="create"><a href="<?php echo $this->config->item('base_url');?>quote/create/"><?php echo $this->lang->line('createquote'); ?></a></li>
+			</ul>
+		</div>
+	    <div class='product-list'>
+	    <?php 
+	    if(isset($error)){
+	    	//var_dump($error);
+	    }
+	    else{ foreach($items as $item):?>
+	       <div class="panel panel-warning">
+	            <div class="panel-heading hidden"><?php echo $item['name']?></div>
+	       		<p><?php echo $item['name']?></p>
+	       		<div class="panel-body">
+				    <div class = 'row hidden'>
+				    	<div class=" col-md-4 col-sm-4 col-xs-4">
+				    	</div>
+				    	<div class=" col-md-8 col-sm-8 col-xs-8 ">
+					    	  <div class = 'row price'>
+						    	  	<div class=" col-md-2 col-sm-3 col-xs-4">
+						    	  		<span class="label label-primary">
+						    	  			<span class="glyphicon glyphicon-usd"></span>价格
+						    	  		</span>
+						    	  	</div>
+					    			<div class=" col-md-10 col-sm-9 col-xs-8 ">
+					    				<?php // echo $product['price']?>
+					    			</div>
+					    	  </div>
+					    	  <div class = 'row address'>
+						    	  	<div class=" col-md-2 col-sm-3 col-xs-4">
+						    	  		<span class="label label-primary">
+						    	  			<span class="glyphicon glyphicon-road"></span>产地
+						    	  		</span>
+						    	  	</div>
+					    			<div class=" col-md-10 col-sm-9 col-xs-8 ">
+					    				<?php //echo isset($product['address']) ? $product['address'] : ''; ?>
+					    			</div>
+					    	  </div>
+					    	  <div class = 'row tag'>
+						    	  	<div class=" col-md-2 col-sm-3 col-xs-4">
+						    	  		<span class="label label-primary">
+						    	  			<span class="glyphicon glyphicon-tag"></span>标签
+						    	  		</span>
+						    	  	</div>
+					    			<div class=" col-md-10 col-sm-9 col-xs-8 ">
+					    				<?php //echo isset($product['tag']) ? $product['tag'] : '';?>
+					    			</div>
+					    	  </div>
+				    	</div>
+				    </div>
+				</div>
+ 		   		<div class="panel-footer">
+ 		   			<a href = 'inquiry/update/id/<?php echo $item['id'] ;?>'  class="btn btn-success btn-mini"><i class="icon-white icon-pencil"></i> <?php echo $this->lang->line('edit') ; ?> </a>
+ 		   		</div>
+	       </div>
+	       <?php endforeach;}?>
+	    </div>
+    </div>
